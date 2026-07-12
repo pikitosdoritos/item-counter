d82a5717148501d9eec2785f770cb3c0dba4de34
pikitosdoritos <fastovetsn@gmail.com>
2026-07-12 20:10:34 +0300

    Add event handlers for item quantity increase and decrease

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const form = document.querySelector('form')
 
 form.onsubmit = handleCalculation
+form.minus.onclick = decrease
+form.plus.onclick = increase
 
 function handleCalculation() {
     const quantity = form.count.value
+}
+
+function decrease() {
+    const output = form.count
+
+    if (output.value <= 0) return alert('The number cannot be negative!')
+
+    output.value = Number(output.value) - 1
+}
+
+function increase() {
+    const output = form.count
+
+    output.value = Number(output.value) + 1
 }
